645d96f39e0541101d1e12e9016d358d2c715fa0
wangqi <850356875@qq.com>
2022-03-24 14:01:39 +0800

    add check node version

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,14 +2,32 @@
 
 module.exports = core;
 
+const semver = require('semver')
+const colors = require('colors/safe')
+
 const pkg = require('../package.json')
 const log = require('@zhazha-cli-dev/log')
+const constants = require('./const')
 
 function core() {
-    checkPackageVersion()
+    try {
+        checkPackageVersion()
+        checkNodeVersion()
+    } catch (e) {
+        log.error(e.message)
+    }
 }
 
 function checkPackageVersion() {
-    console.log(pkg.version)
-    log.success('cli','notice log')
+    log.success('cli', pkg.version)
+}
+
+function checkNodeVersion() {
+    // 获取当前版本
+    const currentVersion = process.version
+    const lowestVersion = constants.LOWEST_NODE_VERSION
+    // 和最低版本比较
+    if (!semver.gte(currentVersion, lowestVersion)) {
+        throw new Error(colors.red(`Node 版本过低，请使用大于${lowestVersion}的版本`))
+    }
 }
